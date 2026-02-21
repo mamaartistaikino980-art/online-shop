2cdc4b6b2d616c0afd2a9656081ec68ea66e7adf
osman <edramina@mail.ru>
2026-02-21 20:29:14 +0500

    lesson 25

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const iPhone = document.querySelector('iPhone')
-const samsung = document.querySelector('samsung')
+const iPhone = document.querySelector('.iphone')
+const samsung = document.querySelector('.samsung')
 
 iPhone.addEventListener('click', function(event){
  let clickTarget = event.target;
@@ -24,5 +24,31 @@ if (currentPrice === "medium") {
 
 if (currentPrice === "large") {
     priceItem.textContent = "Цена 100 000р.";
+}
+});
+
+samsung.addEventListener('click', function(event){
+ let clickTarget = event.target;
+    let activeButton = samsung.querySelector(".active");
+    let priceItem = samsung.querySelector(".price");
+    let currentPrice;
+    let activeButtonColor = samsung.querySelector(".active-border");
+
+     if (clickTarget.classList.contains("color-btn") && !clickTarget.classList.contains("active-border")) {
+        clickTarget.classList.add("active-border");
+        activeButtonColor.classList.remove("active-border");
+    }
+
+if (clickTarget.classList.contains("btn") && !clickTarget.classList.contains("active")) {
+    clickTarget.classList.add("active");
+    activeButton.classList.remove("active");
+}
+currentPrice = clickTarget.getAttribute("data-size");
+if (currentPrice === "medium") {
+    priceItem.textContent = "Цена 40 000р.";
+}
+
+if (currentPrice === "large") {
+    priceItem.textContent = "Цена 59 000р.";
 }
 });
